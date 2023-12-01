9f8995cad8a3157fbce6f0e9fada76d3e67c1368
WaitInTheBush <theo.ollivier@gmail.com>
2023-12-01 11:34:43 +0100

    Etape2

---
+++ TPFormes/Cercle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPFormes
+{
+    public class Cercle
+    {
+        public Cercle(Point origine, int rayon)
+        {
+            Origine = origine;
+            Rayon = rayon;
+        }
+
+        public Point Origine { get; set; }
+        public int Rayon { get; set; }
+
+        public double Perimetre()
+        {
+            return Math.PI * Rayon * 2;
+        }
+
+        public double Aire()
+        {
+            return Math.PI * Rayon * Rayon;
+        }
+
+        public override string? ToString()
+        {
+            return "Cercle :\n" +
+                $"\tOrigine: {Origine}\n" +
+                $"\tRayon: {Rayon}\n" +
+                $"\tPerimetre: {Perimetre()}\n" +
+                $"\tAire: {Aire()}";
+        }
+    }
+}
--- TPFormes/Program.cs
+++ TPFormes/Program.cs
@@ -3,6 +3,8 @@ using TPFormes;
 
 // test du centre du repère
 Point P0 = new Point();
+// étape 1
+
 Console.WriteLine(P0);
 
 // test création d'un point quelconque
@@ -14,3 +16,13 @@ Console.WriteLine(P1);
 Point P2 = new Point();
 Console.WriteLine(P2.Equals(P0)); // doit écrire true
 Console.WriteLine(P2.Equals(P1)); // doit écrire false
+
+// étape 2
+List<Cercle> cercles = new List<Cercle>();
+cercles.Add(new Cercle(P0, 5));
+cercles.Add(new Cercle(P1, 78));
+cercles.Add(new Cercle(P2, 2));
+foreach (Cercle cercle in cercles)
+{
+    Console.WriteLine(cercle);
+}
