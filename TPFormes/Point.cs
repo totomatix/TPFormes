defd40992dacd38dd26fb3991c6f38957028ee51
WaitInTheBush <theo.ollivier@gmail.com>
2023-12-01 11:22:58 +0100

    Ajoutez des fichiers projet.

---
+++ TPFormes/Point.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPFormes
+{
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point()
+        {
+            X = 0;
+            Y = 0;
+        }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string? ToString()
+        {
+            return $"(X : {X}, Y : {Y})";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Point point &&
+                   X == point.X &&
+                   Y == point.Y;
+        }
+    }
+}
---
+++ TPFormes/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using TPFormes;
+
+// test du centre du repère
+Point P0 = new Point();
+Console.WriteLine(P0);
+
+// test création d'un point quelconque
+Point P1= new Point(-8,5);
+Console.WriteLine(P1);
+
+// test de equals
+// création d'un 3eme point egale à P0
+Point P2 = new Point();
+Console.WriteLine(P2.Equals(P0)); // doit écrire true
+Console.WriteLine(P2.Equals(P1)); // doit écrire false
